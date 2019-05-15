73f8b3d08c0bf99d5d29679a059633217234e000
awaritoma <cawaritoma@yahoo.com>
2019-05-15 14:53:42 +0100

    my first DOM commit

---
+++ script.js
@@ -0,0 +1,159 @@
+windows.location.href = "index2.html"; // used to redirect to another html file
+
+
+
+/*document.write("documentwrite class day3");
+/*F 0 - 39
+D 40 - 49
+C 50 - 59
+B 60 - 69
+A 70 - 100*/
+/*document.write("<br>");
+
+
+var examScore = 101;
+
+if (examScore >= 70 && examScore <= 100){
+	document.write("you got an A:" + examScore);
+}
+else if(examScore >= 60 && examScore <= 69){
+	documentwrite("you got a B:" + examScore);
+}
+else if(examScore >= 50 && examScore <= 59){
+	documentwrite("you got a C:" + examScore);
+}
+else{
+	document.write("invalid Exam Score");
+}
+document.write("<br>");
+
+function addition(num1, num2, num3, num4){
+	document.write("Add:" + (num1 + num2 + num3 + num4) + "<br>");
+	document.write(num1 * num4 + "<br>");
+}
+addition(3, 5, 6, 10);
+addition(1, 2, 3, 4);
+document.write("<br>");
+
+A = 20
+B = 26
+H = 15
+
+function trapezium(A, B, H){
+	document.write("solve:" + (A + B) / 2 * H)
+}
+trapezium(20, 26, 15);
+document.write("<br>");
+
+function breakfast(lunch){
+	document.write("ogi and Akara<br>");
+	lunch();
+}
+function lunch(){
+	document.write("bread and butter<br>");
+}
+breakfast(lunch);
+
+var movieList = {
+	"movie1": "Endgame",
+	"movie2": "Birdbox",
+	"movie3": "Alita",
+	"movie4": "CaptainMarvel",
+	"movie5": "Hobbs & Shaw",
+	"movie6": "John Wick",
+	"Year": 2019,
+	"bool": true,
+	"rating": null
+
+
+};
+movieList.movie7 = "GOT S8";
+movieList.actor = "John Doe";
+console.log(movieList);
+console.log(movieList.movie4);
+document.write("<br>");
+
+var countingNumbers = [{"one": 1, "two": 2}, {"three": 3, "four": 4}]
+console.log(countingNumbers[1].four);
+console.log(countingNumbers[0].two);
+document.write("<br>");*/
+
+/*var lorem = document.getElementById("lorem");
+lorem.style.backgroundColor = "green";
+lorem.style.fontSize = "20px";
+lorem.innerHTML = "Hello, how are you doing";
+var bio = document.getElementsByClassName("bio");
+bio[0].innerHTML = "Name: James Bond";
+bio[1].style.backgroundColor = "purple";
+bio[1].style.color = "white";
+bio[3].style.backgroundColor = "black";
+bio[3].style.color = "yellow";
+bio[0].style.backgroundColor = "red";
+bio[2].style.backgroundColor = "indigo";
+
+var loremSelector = document.querySelector("#lorem");
+loremSelector.style.backgroundColor = "lightgreen";
+
+var bioSelector = document.querySelectorAll(".bio");
+bioSelector[0].style.backgroundColor = "blue";
+document.write("<br>");
+alert("good afternoon");
+document.write("<br>");
+var day = "morning";
+if (day == "morning"){
+	alert("good morning");
+}
+else if (day == "afternoon"){
+	alert("good afternoon");
+}
+else if (day == "evening"){
+	alert("good evening");
+}
+else{
+	alert("cannot get time of the day");
+}
+document.write("<br>");
+confirm("are u sure you want to delete");
+document.write("<br>");
+/*var deleteConfirmation = confirm("are u sure you want to delete");
+console.log(deleteConfirmation){
+	alert("file deleted");
+}*/
+/*document.write("<br>");
+function name(){
+	console.log("james bond");
+}
+//setTimeout(name, 3000);
+//setInterval(name, 2000);*/
+
+/*document.write("<br>");
+
+function name(){
+	document.write("james bond");
+setInterval(name, 2000);
+document.write("<br>");*/
+
+document.write("hello world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
